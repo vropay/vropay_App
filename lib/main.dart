162a162fcea1c1617a00e2e73a6f80e21ce0f56f
Digesh Kapadiya <digesh.belovance@gmail.com>
2025-08-08 16:58:29 +0530

    project handover by digesh

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 
 import 'Themes/themes.dart';
